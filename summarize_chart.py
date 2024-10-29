d863657f72d40e7dd3ad00bea8eb7d82fda48539
fishluv <110374653+fishluv@users.noreply.github.com>
2024-10-29 00:17:00 -0500

    less weird language

--- summarize_chart.py
+++ summarize_chart.py
@@ -70,11 +70,11 @@ def summarize_chart(bin_filename, new_format):
   duration_by_bpm[last_bpm] += duration_ms - last_bpm_ts
   bpm_primary, bpm_primary_duration = sorted(duration_by_bpm.items(), key=itemgetter(1), reverse=True)[0]
   if bpm_primary_duration == duration_ms:
-    bpm_primary_type = "totality"
+    bpm_primary_type = "constant"
   elif bpm_primary_duration > duration_ms / 2:
     bpm_primary_type = "majority"
   else:
-    bpm_primary_type = "plurality"
+    bpm_primary_type = "nonmajority"
 
   # Ensure this is sorted by timestamp.
   # (This is not guaranteed since it is possible for smaller timestamps
