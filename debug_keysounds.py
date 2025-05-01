0eac8527233bfb2ecc8e78c6e990e2524219d603
fishluv <110374653+fishluv@users.noreply.github.com>
2025-05-01 13:43:01 -0700

    add option to ignore sample index 0

--- debug_keysounds.py
+++ debug_keysounds.py
@@ -12,11 +12,13 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--bin-file", required=True)
   parser.add_argument("--no-source", action="store_true")
+  parser.add_argument("--no-zero", action="store_true")
   parser.add_argument("--sort-in-timestamp", action="store_true")
   args = parser.parse_args()
 
   bin_file = args.bin_file
   no_source = args.no_source
+  no_zero = args.no_zero
   sort_in_timestamp = args.sort_in_timestamp
 
   played_samples = []
@@ -24,6 +26,8 @@ if __name__ == "__main__":
   for timestamp, event_name, event_value, _ in get_events(bin_file):
     if event_name == "key":
       key = event_value
+      if no_zero and loaded_keysound_for_key[key] == 0:
+        continue
       played_samples.append(((timestamp, loaded_keysound_for_key[key], "key%s" % key)))
 
     if event_name == "loadsample" and event_value & 0xff != 0:
@@ -32,6 +36,8 @@ if __name__ == "__main__":
 
     if event_name == "playsample":
       sample_idx = event_value & 0xff
+      if no_zero and sample_idx == 0: # rare and idk why but it happens
+        continue
       played_samples.append((timestamp, sample_idx, "playsample"))
 
   if sort_in_timestamp:
